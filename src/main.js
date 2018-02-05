338463c912cb3731a784af2e279cee9d6590aad0
zhoujialin <724475200@qq.com>
2018-02-05 16:15:39 +0800

    next

--- src/main.js
+++ src/main.js
@@ -1,13 +1,39 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+import { Observable } from 'rxjs/Observable'
+/**
+ * fruitsObservable表示数据的生产者
+ * next是交付数据信息
+ * error是交付错误信息
+ * complete是通知没有订阅者没有交付的数据了
+ * error和complete这2个只能发生其中一个，不论那个发生，都会中断执行的数据流
+ * pull push 表示的数据的提取和推送
+ */
+const fruitsObservable = Observable.create(observer => {
+  observer.next('apple')
+  observer.next('orage')
+  // observer.error(new Error('someone took my fruit.'))
+  setTimeout(() => {
+    observer.next('lem')
+    observer.complete()
+  }, 2000)
 })
+/*
+  数据的使用者
+ */
+const fruitsObserver = {
+  next: data => console.log(data),
+  error: error => console.log(error.message),
+  complete: () => console.log('done!')
+}
+
+console.log('----- before subscribe -----')
+/*
+  订阅数据
+ */
+const fruitsSubscription = fruitsObservable.subscribe(fruitsObserver)
+console.log('----- after subscribe -----')
+/*
+  取消订阅数据
+ */
+setTimeout(() => {
+  fruitsSubscription.unsubscribe()
+}, 1000)
